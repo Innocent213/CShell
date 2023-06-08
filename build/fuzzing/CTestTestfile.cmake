c4ba5dd329f1b4757500abcc207b6fcef3bf8961
Innocent <colarossi_3006.luca@snets.it>
2023-06-08 17:48:15 +0200

    Commit

--- build/fuzzing/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntuschule/Documents/C_Programme/CShell/lib/cJSON/fuzzing
-# Build directory: /home/ubuntuschule/Documents/C_Programme/CShell/build/fuzzing
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
