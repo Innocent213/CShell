44a259710d74041a86d307c756e419f18ac2141b
Innocent <colarossi_3006.luca@snets.it>
2023-06-09 08:00:22 +0200

    Commit

--- Compile.sh
+++ Compile.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 clear
+if [ $EUID -eq 0 ]; then
+    chmod 777 Cleanup.sh
+    chmod 777 StartCShell.sh
+    chmod 777 Utils/CompileUtils.sh
+    chmod 777 Utils/StartNSSHClient.sh
+    chmod 777 scripts/InstallDependencies.sh
+    chmod 777 scripts/Install_cJSON.sh
+    bash scripts/InstallDependencies.sh
+    bash scripts/Install_cJSON.sh
 
-chmod 777 Cleanup.sh
-chmod 777 StartCShell.sh
-chmod 777 Utils/CompileUtils.sh
-chmod 777 Utils/StartNSSHClient.sh
-chmod 777 scripts/InstallDependencies.sh
-chmod 777 scripts/Install_cJSON.sh
-bash scripts/InstallDependencies.sh
-bash scripts/Install_cJSON.sh
+    gcc -c lib/CSHLib.c -o lib/CSHLib.o -I./cJSON -L./cJSON -lcjson
+    ar rcs lib/libCSHLib.a lib/CSHLib.o
 
-gcc -c lib/CSHLib.c -o lib/CSHLib.o -I../lib/cJSON -L../lib/cJSON -lcjson
-ar rcs lib/libCSHLib.a lib/CSHLib.o
+    gcc -c lib/NSSHLib.c -o lib/NSSHLib.o -I../lib/cJSON -L../lib/cJSON -lcjson -lcrypto -lssl
+    ar rcs lib/libNSSHLib.a lib/NSSHLib.o
 
-gcc -c lib/NSSHLib.c -o lib/NSSHLib.o -I../lib/cJSON -L../lib/cJSON -lcjson -lcrypto -lssl
-ar rcs lib/libNSSHLib.a lib/NSSHLib.o
-
-gcc CShell.c -o CShell -I../lib/cJSON -L../lib/cJSON -L./lib/ -lNSSHLib -lcjson -lCSHLib -lreadline -lcrypto -lssl
-cd Utils
-./CompileUtils.sh
+    gcc CShell.c -o CShell -I./lib/cJSON -L./lib/cJSON -L./lib/ -lNSSHLib -lcjson -lCSHLib -lreadline -lcrypto -lssl
+    cd Utils
+    ./CompileUtils.sh
+else
+  echo "Please execute this Script with Administratorrights."
+fi
--- scripts/InstallDependencies.sh
+++ scripts/InstallDependencies.sh
@@ -1,72 +1,68 @@
 #!/bin/bash
 
-if [ $EUID -eq 0 ]; then
-    INSTALLED_PACKAGE=false
-    PACKAGE_NAME="gcc"
-    dpkg -l $PACKAGE_NAME | grep -e "^ii" > intall_cache.txt
-    if [ -z "$(cat "intall_cache.txt")" ]; then
-        echo "Installing $PACKAGE_NAME..."
-        apt-get -qq -y install $PACKAGE_NAME >/dev/null 2>&1
-        echo "$PACKAGE_NAME successfully installed!"
-        INSTALLED_PACKAGE=true
-    else
-        echo "$PACKAGE_NAME is already installed!"
-    fi
-
-    PACKAGE_NAME="libssl-dev"
-    dpkg -l $PACKAGE_NAME | grep -e "^ii" > intall_cache.txt
-    if [ -z "$(cat "intall_cache.txt")" ]; then
-        echo "Installing $PACKAGE_NAME..."
-        apt-get -qq -y install $PACKAGE_NAME >/dev/null 2>&1
-        echo "$PACKAGE_NAME successfully installed!"
-        INSTALLED_PACKAGE=true
-    else
-        echo "$PACKAGE_NAME is already installed!"
-    fi
-
-    PACKAGE_NAME="libreadline-dev"
-    dpkg -l $PACKAGE_NAME | grep -e "^ii" > intall_cache.txt
-    if [ -z "$(cat "intall_cache.txt")" ]; then
-        echo "Installing $PACKAGE_NAME..."
-        apt-get -qq -y install $PACKAGE_NAME >/dev/null 2>&1
-        echo "$PACKAGE_NAME successfully installed!"
-        INSTALLED_PACKAGE=true
-    else
-        echo "$PACKAGE_NAME is already installed!"
-    fi
-
-    PACKAGE_NAME="git"
-    dpkg -l $PACKAGE_NAME | grep -e "^ii" > intall_cache.txt
-    if [ -z "$(cat "intall_cache.txt")" ]; then
-        echo "Installing $PACKAGE_NAME..."
-        apt-get -qq -y install $PACKAGE_NAME >/dev/null 2>&1
-        echo "$PACKAGE_NAME successfully installed!"
-        INSTALLED_PACKAGE=true
-    else
-        echo "$PACKAGE_NAME is already installed!"
-    fi
-
-    if [ $INSTALLED_PACKAGE ]; then
-        rm intall_cache.txt
-    fi
-    # PACKAGE_NAME="git cmake"
-    # if ! dpkg -s $PACKAGE_NAME >/dev/null 2>&1; then
-    #     echo "Installing $PACKAGE_NAME..."
-    #     apt-get -qq -y install $PACKAGE_NAME >/dev/null 2>&1
-    #     echo "$PACKAGE_NAME successfully installed!"
-    # fi
-
-    # if ! [ -d "lib/libiconv" ]; then
-    #     cd lib
-    #     wget http://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.11.tar.gz
-    #     tar -xvzf libiconv-1.11.tar.gz
-    #     mv libiconv-1.11 libiconv
-    #     cd libiconv
-    #     ./configure
-    #     make
-    #     cd ..
-    #     rm libiconv-1.11.tar.gz
-    # fi
+INSTALLED_PACKAGE=false
+PACKAGE_NAME="gcc"
+dpkg -l $PACKAGE_NAME | grep -e "^ii" > intall_cache.txt
+if [ -z "$(cat "intall_cache.txt")" ]; then
+    echo "Installing $PACKAGE_NAME..."
+    apt-get -qq -y install $PACKAGE_NAME >/dev/null 2>&1
+    echo "$PACKAGE_NAME successfully installed!"
+    INSTALLED_PACKAGE=true
 else
-  echo "Please execute this Script with Administratorrights."
+    echo "$PACKAGE_NAME is already installed!"
 fi
+
+PACKAGE_NAME="libssl-dev"
+dpkg -l $PACKAGE_NAME | grep -e "^ii" > intall_cache.txt
+if [ -z "$(cat "intall_cache.txt")" ]; then
+    echo "Installing $PACKAGE_NAME..."
+    apt-get -qq -y install $PACKAGE_NAME >/dev/null 2>&1
+    echo "$PACKAGE_NAME successfully installed!"
+    INSTALLED_PACKAGE=true
+else
+    echo "$PACKAGE_NAME is already installed!"
+fi
+
+PACKAGE_NAME="libreadline-dev"
+dpkg -l $PACKAGE_NAME | grep -e "^ii" > intall_cache.txt
+if [ -z "$(cat "intall_cache.txt")" ]; then
+    echo "Installing $PACKAGE_NAME..."
+    apt-get -qq -y install $PACKAGE_NAME >/dev/null 2>&1
+    echo "$PACKAGE_NAME successfully installed!"
+    INSTALLED_PACKAGE=true
+else
+    echo "$PACKAGE_NAME is already installed!"
+fi
+
+PACKAGE_NAME="git"
+dpkg -l $PACKAGE_NAME | grep -e "^ii" > intall_cache.txt
+if [ -z "$(cat "intall_cache.txt")" ]; then
+    echo "Installing $PACKAGE_NAME..."
+    apt-get -qq -y install $PACKAGE_NAME >/dev/null 2>&1
+    echo "$PACKAGE_NAME successfully installed!"
+    INSTALLED_PACKAGE=true
+else
+    echo "$PACKAGE_NAME is already installed!"
+fi
+
+if [ $INSTALLED_PACKAGE ]; then
+    rm intall_cache.txt
+fi
+# PACKAGE_NAME="git cmake"
+# if ! dpkg -s $PACKAGE_NAME >/dev/null 2>&1; then
+#     echo "Installing $PACKAGE_NAME..."
+#     apt-get -qq -y install $PACKAGE_NAME >/dev/null 2>&1
+#     echo "$PACKAGE_NAME successfully installed!"
+# fi
+
+# if ! [ -d "lib/libiconv" ]; then
+#     cd lib
+#     wget http://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.11.tar.gz
+#     tar -xvzf libiconv-1.11.tar.gz
+#     mv libiconv-1.11 libiconv
+#     cd libiconv
+#     ./configure
+#     make
+#     cd ..
+#     rm libiconv-1.11.tar.gz
+# fi
