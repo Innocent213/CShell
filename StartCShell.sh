b4b8dd59bc7429a8943f21e7de268df6a3ecf10a
Innocent <colarossi_3006.luca@snets.it>
2023-06-09 08:13:21 +0200

    Commit

--- StartCShell.sh
+++ StartCShell.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 clear
 
-CJSON_PATH=$(readlink -f "lib/cJSON")
-LD_LIBRARY_PATH_VAR="$CJSON_PATH:"
+CJSON_PATH=$(readlink -f "./lib/cJSON")
+if ! [ -z $CJSON_PATH ]; then
+    LD_LIBRARY_PATH_VAR="$CJSON_PATH:"
 
-if [[ $(echo $LD_LIBRARY_PATH) == $LD_LIBRARY_PATH_VAR""$LD_LIBRARY_PATH_VAR ]]; then
-    ./CShell
+    if [[ $(echo $LD_LIBRARY_PATH) == $LD_LIBRARY_PATH_VAR""$LD_LIBRARY_PATH_VAR ]]; then
+        ./CShell
+    else
+        echo Please execute this command and try again: 
+        echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH_VAR""$LD_LIBRARY_PATH_VAR"
+    fi
 else
-    echo Please execute this command and try again: 
-    echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH_VAR""$LD_LIBRARY_PATH_VAR"
+    echo Cannot get path of the cJSON Folder, please reboot your system to make it work!
 fi
-
